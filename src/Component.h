1160d9ae47bc8bdebf12ff84cea48d3681a69521
TheUselessHuman <palinkas.lorinc@gmail.com>
2024-04-01 19:55:14 +0200

    Final initial code documentation

--- src/Component.h
+++ src/Component.h
@@ -42,41 +42,103 @@ public:
 };
 class InPin_Component : virtual public Component {
 protected:
+  /**
+   * @brief A bemeneti pin-ek száma.
+   */
   size_t inPinCount;
+  /**
+   * @brief Az aktív, valid adattal rendelkező pin-ek száma.
+   */
   size_t activeInPins;
+  /**
+   * @brief A bemeneti pin-ek tömbje. Az index jelentést a specifikált alkatrész adja meg.
+   */
   InPin* inPins;
 
 public:
+  /**
+   * @brief Létrehozza a bemeneti pin-ek tömbjét.
+   *
+   * @param inCount A bemeneti pin-ek száma.
+   */
   InPin_Component(size_t inCount);
 
+  /**
+   * @brief Reseteli az aktív pin-ek számát 0-ra.
+   *
+   */
   void resetActiveCount() { activeInPins = 0; }
+
+  /**
+   * @brief Növeli az aktív lábak számát, és aktivizálja az áramköri elemet, ha eléri a maximumot.
+   *
+   */
   void tickCounter();
 
+  /**
+   * @brief Végrehajtja a funkcióját az áramköri elemnek. Leszármazottban konkretizálva.
+   *
+   */
   virtual void executeFunction() = 0;
 
+  /**
+   * @brief Törli a bemeneti pin-ek tömbjét.
+   *
+   */
   virtual ~InPin_Component() { delete[] inPins; }
 
 };
 class OutPin_Component : virtual public Component {
 protected:
+  /**
+   * @brief A kimeneti pin-ek száma.
+   */
   size_t outPinCount;
-
+  /**
+   * @brief A kimeneti pin-ek tömbje.  Az index jelentést a specifikált alkatrész adja meg.
+   *
+   */
   OutPin* outPins;
 
 public:
+  /**
+   * @brief Létrehozza a kimeneti pin-ek tömbjét.
+   *
+   * @param outCount A kívánt kimeneti pin-ek száma.
+   */
   OutPin_Component(size_t outCount);
 
+  /**
+   * @brief Kiküldi minden kimeneti lábán a lábakban tárolt jeleket.
+   *
+   */
   void sendOutSignals();
 
+  /**
+   * @brief Végrehajtja a funkcióját az áramköri elemnek. Leszármazottban konkretizálva.
+   *
+   */
   virtual void executeFunction() = 0;
 
+  /**
+   * @brief Törli a kimeneti pin-ek tömbjét.
+   *
+   */
   virtual ~OutPin_Component() { delete[] outPins; }
 
 };
 class IOPin_Component : public InPin_Component, public OutPin_Component {
 public:
+  /**
+   * @brief Végrehajtja a funkcióját az áramköri elemnek. Leszármazottban konkretizálva.
+   *
+   */
   virtual void executeFunction() = 0;
 
+  /**
+   * @brief Virtuális destruktor, mert öröklés.
+   *
+   */
   virtual ~IOPin_Component() {}
 };
 #endif
